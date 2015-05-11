8907daa0c0da343a246a17229c27f451fcc53bb3
phillib <phillib@users.noreply.github.com>
2015-05-11 16:43:09 -0500

    Update final_shopping_list.py

--- Python-Collections/final_shopping_list.py
+++ Python-Collections/final_shopping_list.py
@@ -12,7 +12,8 @@ def remove_item(idx):
 def show_help():
   print("\nSeparate each item with a comma.")
   print("Type DONE to quit, SHOW to see the current list, "
-        "REMOVE to delete an item, and HELP to get this message.")
+        "REMOVE to delete an item, MOVE to move an item, "
+        "CLEAR to empty your list, and HELP to get this message.")
   
   
 def show_list():
@@ -20,6 +21,20 @@ def show_list():
   for item in shopping_list:
     print("{}: {}".format(count, item))
     count += 1
+  if shopping_list == []:
+    print("Your shopping list is currently empty")
+    
+    
+def move_item(to_move, new_position): #Moves an item currently in your list to a new position of your choosing.
+  index = to_move - 1
+  position = new_position - 1
+  shopping_list.insert(position, shopping_list.pop(index))
+  
+  
+def clear_list(): #This targets the global variable for shopping_list and empties its contents
+  global shopping_list
+  shopping_list = []
+  
     
 print("Give me a list of things you want to shop for.")
 show_help()
@@ -42,6 +57,25 @@ while True:
     idx = input("Which item?  Tell me the number. ")
     remove_item(int(idx))
     continue
+  elif new_stuff.lower() == "move":
+    show_list()
+    to_move = input("Which item number would you like to move? ")
+    new_position = input("Great!  Now where on the list would you like to move it? ")
+    move_item(int(to_move), int(new_position))
+    continue
+  elif new_stuff.lower() == "clear":
+    show_list()
+    answer = input("Are you sure you want to clear the current list? (Y/N) ")
+    if answer.lower() == "y":
+      clear_list()
+      print("Your shopping list is now empty.")
+      continue
+    elif answer.lower() == "n":
+      print("Your shopping list is unchanged.")
+      continue
+    else:
+      print("Please enter 'Y' or 'N' as your selection.  Type 'CLEAR' to try again")
+      continue
   else:
     new_list = new_stuff.split(",")
     index = input("Add this at a certain spot? Press enter for the end of the list, "
